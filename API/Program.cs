7066be211171ed217bd163f4e555ddf9db65007b
Daniel Sousa <mdaniel448@outlook.com>
2024-09-28 14:28:47 -0300

    Implemented Docker support

--- API/Program.cs
+++ API/Program.cs
@@ -140,12 +140,9 @@ internal class Program
 
         // Configure the HTTP request pipeline.
         
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-        
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
 
         app.UseHttpsRedirection();
 
